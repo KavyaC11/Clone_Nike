093467104ae77275aa5e14fcb8a71c8ba9782c8f
KavyaC11 <kmoments11@gmail.com>
2024-11-26 14:12:00 +0530

    update3

--- src/App.js
+++ src/App.js
@@ -11,25 +11,58 @@ import WishlistPage from './pages/WishlistPage';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-	return (
-		<>
-			<Router>
-				{/* <Navbar /> You can put Navbar here if you want it to appear on every page */}
-				<Routes>
-					{/* Using element prop to render components */}
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Auth />} />
-					<Route path="/user" element={<User />} />
-					<Route path="/cart" element={<CartPage />} />
-					<Route path="/wish" element={<WishlistPage />} />
-				</Routes>
-			</Router>
-			<ToastContainer />
-		</>
-	);
+  return (
+    <>
+      <Router basename="/Clone_Nike"> {/* Add the basename here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Auth />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/wish" element={<WishlistPage />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
+
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import './App.css';
+// import { useSelector } from 'react-redux';
+// import Auth from './pages/Auth';
+// import User from './pages/User';
+// import CartPage from './pages/CartPage';
+// import WishlistPage from './pages/WishlistPage';
+// import { ToastContainer } from 'react-toastify';
+
+// const App = () => {
+// 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+// 	return (
+// 		<>
+// 			<Router>
+// 				{/* <Navbar /> You can put Navbar here if you want it to appear on every page */}
+// 				<Routes>
+// 					{/* Using element prop to render components */}
+// 					<Route path="/" element={<Home />} />
+// 					<Route path="/login" element={<Auth />} />
+// 					<Route path="/user" element={<User />} />
+// 					<Route path="/cart" element={<CartPage />} />
+// 					<Route path="/wish" element={<WishlistPage />} />
+// 				</Routes>
+// 			</Router>
+// 			<ToastContainer />
+// 		</>
+// 	);
+// };
+
+// export default App;
 
