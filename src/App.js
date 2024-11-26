dd4e495ab647cd85c4ef9cca9b59b8d4bf191734
KavyaC11 <kmoments11@gmail.com>
2024-11-26 11:47:03 +0530

    first

--- src/App.js
+++ src/App.js
@@ -33,37 +33,3 @@ const App = () => {
 
 export default App;
 
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Home from './pages/Home';
-// import './App.css';
-// import { useSelector } from 'react-redux';
-// import Auth from './pages/Auth';
-// import User from './pages/User';
-// import CartPage from './pages/CartPage';
-// import WishlistPage from './pages/WishlistPage';
-// import { ToastContainer } from 'react-toastify';
-
-// const App = () => {
-// 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-
-// 	return (
-// 		<>
-// 			<Router >
-// 				<Routes>
-// 					<Route path='/' exact>
-// 						<Home />
-// 					</Route>
-// 					<Route path='/login' component={Auth} />
-// 					<Route path='/user' component={User} />
-// 					<Route path='/cart' component={CartPage} />
-// 					<Route path='/wish' component={WishlistPage} />
-// 				</Routes>
-// 			</Router>
-// 			<ToastContainer />
-// 		</>
-// 	);
-// };
-
-// export default App;
